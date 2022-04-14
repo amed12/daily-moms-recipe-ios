66a803842b261bb140ada6396f3b125be81c0df8
Achmad Fathullah <amedixro@gmail.com>
2022-04-14 15:28:02 +0700

    add xib only ui

---
+++ DailyMomsRecipe/Pods/Target Support Files/Pods-DailyMomsRecipe-DailyMomsRecipeUITests/Pods-DailyMomsRecipe-DailyMomsRecipeUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DailyMomsRecipe_DailyMomsRecipeUITests : NSObject
+@end
+@implementation PodsDummy_Pods_DailyMomsRecipe_DailyMomsRecipeUITests
+@end
