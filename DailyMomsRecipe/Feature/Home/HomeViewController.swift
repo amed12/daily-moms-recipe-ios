41ce825a741f23c3b5d6ddaf4db46cc2e444c2d5
Achmad Fathullah <amedixro@gmail.com>
2022-04-18 14:08:51 +0700

    add custom segmented control

--- DailyMomsRecipe/Feature/Home/HomeViewController.swift
+++ DailyMomsRecipe/Feature/Home/HomeViewController.swift
@@ -7,23 +7,63 @@
 
 import UIKit
 
+
 class HomeViewController: UIViewController {
 
+    @IBOutlet weak var customSegmentedView: UIView!
+    @IBOutlet weak var headerImgView: UIImageView!
+    var buttonBar : UIView = UIView()
     override func viewDidLoad() {
         super.viewDidLoad()
+        headerImgView.image = UIImage(named: "dining")
+        customSegmentedControl()
 
         // Do any additional setup after loading the view.
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    private func customSegmentedControl(){
+        let segmentedControl = UISegmentedControl()
+        customSegmentedView.backgroundColor = .clear
+        customSegmentedView.translatesAutoresizingMaskIntoConstraints = false
+        // Add segments
+        segmentedControl.insertSegment(withTitle: "One", at: 0, animated: true)
+        segmentedControl.insertSegment(withTitle: "Two", at: 1, animated: true)
+        segmentedControl.insertSegment(withTitle: "Three", at: 2, animated: true)
+        // First segment is selected by default
+        segmentedControl.selectedSegmentIndex = 0
+        // This needs to be false since we are using auto layout constraints
+        segmentedControl.translatesAutoresizingMaskIntoConstraints = false
+        // This needs to be false since we are using auto layout constraints
+        buttonBar.translatesAutoresizingMaskIntoConstraints = false
+        buttonBar.backgroundColor = UIColor.blue
+        customSegmentedView.addSubview(segmentedControl)
+        customSegmentedView.addSubview(buttonBar)
+        // Constrain the segmented control width to be equal to the container view width
+        segmentedControl.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+        // Constraining the height of the segmented control to an arbitrarily chosen value
+        segmentedControl.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        segmentedControl.backgroundColor = .clear
+        segmentedControl.tintColor = .clear
+        segmentedControl.setTitleTextAttributes([
+            NSAttributedString.Key.foregroundColor: UIColor.black,
+            NSAttributedString.Key.font: UIFont.systemFont(ofSize: 16, weight: .regular)], for: .normal)
+        segmentedControl.setTitleTextAttributes([
+            NSAttributedString.Key.foregroundColor: UIColor.blue,
+            NSAttributedString.Key.font: UIFont.systemFont(ofSize: 16, weight: .regular)], for: .selected)
+        segmentedControl.addTarget(self, action: #selector(segmentedControlValueChanged(_:)), for: .valueChanged)
+        // Constrain the top of the button bar to the bottom of the segmented control
+        buttonBar.topAnchor.constraint(equalTo: segmentedControl.bottomAnchor).isActive = true
+        buttonBar.heightAnchor.constraint(equalToConstant: 5).isActive = true
+        // Constrain the button bar to the left side of the segmented control
+        buttonBar.leftAnchor.constraint(equalTo: segmentedControl.leftAnchor).isActive = true
+        // Constrain the button bar to the width of the segmented control divided by the number of segments
+        buttonBar.widthAnchor.constraint(equalTo: segmentedControl.widthAnchor, multiplier: 0.98 / CGFloat(segmentedControl.numberOfSegments)).isActive = true
     }
-    */
+    
+    @objc func segmentedControlValueChanged(_ sender: UISegmentedControl) {
+        UIView.animate(withDuration: 0.1) {
+            self.buttonBar.frame.origin.x = (sender.frame.width / CGFloat(sender.numberOfSegments)) * CGFloat(sender.selectedSegmentIndex)
+          }
+    }
 
 }
